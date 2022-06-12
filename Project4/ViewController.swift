ee682323d2ca2516a40397acbaf7449208f19ff2
Juliano C L Santos <foxports@hotmail.com>
2022-06-12 15:55:17 -0300

    desafio finalizado.

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -7,18 +7,10 @@
 
 import WebKit
 
-class ViewController: UITableViewController, WKNavigationDelegate {
+class ViewController: UITableViewController {
 
-    var webView: WKWebView!
-    var progressView: UIProgressView!
     var websites = ["apple.com", "hackingwithswift.com", "google.com"]
 
-    /*override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }*/
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Site Viewer"
@@ -35,82 +27,15 @@ class ViewController: UITableViewController, WKNavigationDelegate {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        goNavigation(websites[indexPath.row])
-    }
-    
-    func goNavigation(_ website: String) {
-        
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-        
-        //navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-
-        progressView = UIProgressView(progressViewStyle: .default)
-        progressView.sizeToFit()
-        let progressButton = UIBarButtonItem(customView: progressView)
-        
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        let goBack = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView.goBack, action: #selector(webView.goBack))
-        let goForward = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView.goForward, action: #selector(webView.goForward))
-      
-        toolbarItems = [progressButton, spacer, goBack, goForward, refresh]
-        navigationController?.isToolbarHidden = false
-        
-        let url = URL(string: "https://" + website)!
-        webView.load(URLRequest(url: url))
-        
-        webView.allowsBackForwardNavigationGestures = true
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-    }
-    
-    @objc func openTapped(website: String) {
-        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
-
-        ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
-        
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
-        present(ac, animated: true)
-    }
-    
-    
-    func openPage(action: UIAlertAction) {
-        let url = URL(string: "https://" + action.title!)!
-        webView.load(URLRequest(url: url))
-    }
-    
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
-    }
-    
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "estimatedProgress" {
-            progressView.progress = Float(webView.estimatedProgress)
+        //goNavigation(websites[indexPath.row])
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "WebBrowser") as? WebBrowserViewController {
+            vc.websites = self.websites
+            vc.website = websites[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
         }
     }
     
-    
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        let url = navigationAction.request.url
-
-        if let host = url?.host {
-            for website in websites {
-                if host.contains(website) {
-                    decisionHandler(.allow)
-                    return
-                }
-            }
-        }
-        
-        let ac = UIAlertController(title: "Acesso não permitido", message: "O site que você está tentando acessar não está na lista de sites seguros!", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Ok", style: .default))
-        present(ac, animated: true)
-
-        decisionHandler(.cancel)
-    }
+  
 }
 
